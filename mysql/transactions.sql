80df7abeace18550240e8f244e183a23bcd10bc0
Srivoknovski <test@gmail.com>
2018-12-03 01:32:25 +0200

    mysql

--- mysql/transactions.sql
+++ mysql/transactions.sql
@@ -3,3 +3,13 @@ SHOW VARIABLES LIKE "AUTOCOMMIT";
 SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED ;
 
 create simple database !
+https://downloads.mysql.com/docs/employee-en.pdf
+
+start TRANSACTION;
+update, DELETE
+commit by websocket
+
+set open id for google;
+
+
+https://www.tornadoweb.org/en/stable/auth.html
