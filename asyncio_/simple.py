46b38479074f153767d20706edc5e382773e36f2
andrei <an@gmail.com>
2019-03-28 20:50:57 +0200

    receipt

--- asyncio_/simple.py
+++ asyncio_/simple.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import asyncio
 
 import aiohttp
@@ -6,8 +7,14 @@ import aiohttp
 # f = aiofiles.open()
 
 #aiofiles.open('filename', mode='r') as f:
+from bs4 import BeautifulSoup
 
-file = open('/home/andrei/Python/sqlalchemy-lab/receipt/catalog.txt', 'r')
+pd.set_option('display.height', 1000)
+pd.set_option('display.max_rows', 50)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 2000)
+pd.set_option('display.line_width', 2000)
+file = open('/home/andrei/Python/sqlalchemy-lab/receipt/links.txt', 'r')
 
 # def get_next_file_line():
 #     yield from loop.run_in_executor(None, file.readline)
@@ -20,24 +27,72 @@ async def foo():
     await asyncio.sleep(0)
     print('Explicit context switch to foo again')
 
-
+base_url = "https://www.gastronom.ru"
 async def parse_web_page(url, session):
-    print('parse_web_page', url)
-    async with session.get("https://www.gastronom.ru"+url, timeout=60) as response:
+    if url[0] != '/':
+        url = '/' + url
+
+    url = base_url + url.strip()
+
+    async with session.get(url, timeout=60) as response:
         res = await response.text()
-        print('parse_web_page', url, 'got', len(res))
-        return len(res)
+        soup = BeautifulSoup(res)
+
+        title = soup.find('h1', {'class': 'recipe__title'}).text
+        preview = base_url + soup.find('img', {'class':'result-photo'})['src']
+        ingredients = [item.text for item in soup.find_all('li', {'class':'recipe__ingredient'})]
+
+        list_titles = [item.text.strip() for item in soup.find_all('div', {'class': 'recipe__summary-list-title'})]
+        list_desc = [item.text.strip() for item in soup.find_all('div', {'class': 'recipe__summary-list-des'})]
+
+        summary = list(zip(list_titles, list_desc))
+
+        desc = [item.text.strip() for item in soup.find_all('div', {'class': 'recipe__step-text'})]
+
+        return (title, url, preview, ingredients, summary, desc)
 
 
 async def fetch_all_urls(session, loop):
-    return await asyncio.gather(*[parse_web_page(url, session) for url in await get_next_file_line()], return_exceptions=True)
+    c = 1000
+    n = 0
+
+    data = [parse_web_page(await get_next_file_line(), session) for i in range(c)]
+
+    while len(data) == c:
+        res = await asyncio.gather(*data, return_exceptions=True)
+        print('Iteration', n)
+        print(res)
+        n += 1
+        process_data(res)
+        data = []
+        for i in range(c):
+            url = await get_next_file_line()
+
+            if not url:
+                break
+
+            data.append(parse_web_page(url, session))
+
+    res = await asyncio.gather(*data, return_exceptions=True)
+    process_data(res, True)
+    return res
+    # return await asyncio.gather(*[parse_web_page(url, session) for url in await get_next_file_line()], return_exceptions=True)
+
+coocking_data = []
+i = 1
+def process_data(res, finish=False):
+    global coocking_data, i
+
+    coocking_data += res
+
+    if len(coocking_data) >= 5000 or finish:
+        df = pd.DataFrame(columns=['title', 'url', 'preview', 'ingredients', 'summary', 'desc'], data=coocking_data)
+        df.to_pickle('/home/andrei/Python/sqlalchemy-lab/receipt/gastronom/receipts_{}.pkl'.format(i))
+        i += 1
+        coocking_data = []
 
 
-urls = [
-    'https://povar.ru/recipes/manty_po-uzbekski-18397.html',
-    'https://povar.ru/recipes/litovskie_ceppeliny-8617.html',
-    'https://povar.ru/recipes/podliv_iz_svininy-7633.html',
-]
+
 
 loop = asyncio.get_event_loop()
 connector = aiohttp.TCPConnector(limit=100)
@@ -46,5 +101,3 @@ htmls = loop.run_until_complete(fetch_all_urls(session, loop))
 loop.run_until_complete(session.close())
 loop.close()
 file.close()
-
-print(htmls)
